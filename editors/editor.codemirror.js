786d32d4de5d0e0288c2aa6a0d5d02955ee7f426
crccheck <c@crccheck.com>
2013-03-11 16:32:17 -0500

    add codemirror

---
+++ editors/editor.codemirror.js
@@ -0,0 +1,34 @@
+// custom texas tribune implementation of CodeMirror
+
+/*globals $, STATIC_URL, EDITOR_CSS, CodeMirror */
+
+(function(exports, superTextareas){
+  "use strict";
+
+  window.superTextareas.addEditor({
+    name: 'CodeMirror',
+    isInstalled: function(){ return typeof CodeMirror !== "undefined"; },
+    css: [STATIC_URL + 'codemirror/lib/codemirror.css'],
+    js: [STATIC_URL + 'codemirror/codemirror-compressed.js'],
+    enable: function(textarea){
+      var $textarea = $(textarea),
+          dim = {
+            height: $textarea.height(),
+            width: $textarea.width()
+          };
+      var options = {
+        lineNumbers: true,
+        tabSize: 2
+      };
+      var cm = CodeMirror.fromTextArea(textarea, options);
+      cm.setSize(dim.width, dim.height);
+      $textarea.data('_ed_cm', cm);
+    },
+    disable: function(textarea){
+      var ed_ref = $(textarea).data('_ed_cm');
+      ed_ref.save();
+      ed_ref.toTextArea();
+    }
+  });
+
+})(window, superTextareas);
